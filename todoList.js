e9f175b572a8f8b467123b1cb29a01d5c7694d1e
sjakedude <sjakedude@gmail.com>
2020-11-22 16:10:23 -0500

    Adding js and vue

---
+++ todoList.js
@@ -0,0 +1,19 @@
+const app = new Vue({
+    el: '#taskList',
+    data: {
+        tasks: ["Poop"]
+    },
+    created() {
+        fetch("https://api.myjson.com/bins/74163")
+            .then(response => response.json())
+            .then(json => {
+                this.products = json.products
+            })
+    }
+})
+
+function changeTask() {
+    let task = document.getElementById("task").value
+    app.tasks.push(task)
+    alert("Task added");
+}
