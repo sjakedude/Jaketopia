f946aee85e0dc85f30c34f32b8e92e58d2685f68
sjakedude <sjakedude@gmail.com>
2022-01-23 19:43:52 -0500

    cleanup

--- js/resume.js
+++
@@ -1,7 +0,0 @@
-function displayResume() {
-    if (confirm("Resume will open another window")) {
-        window.open("media/resume.pdf", "Resume", "toolbar=no, location=no");
-    } else {
-        return false;
-    }
-}
